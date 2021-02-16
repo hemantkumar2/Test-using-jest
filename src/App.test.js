0cc69dceb55f27c6f3cd9b2375fd87b498d8e603
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-02-16 18:35:08 +0530

    changed component names

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("Render without error", () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[href='http://nirmalkar.com']");
-  expect(appComponent.length).toBe(1);
+  const link = wrapper.find("[href='http://nirmalkar.com']");
+  expect(link.length).toBe(1);
 });
 
 test("Check for some component attribute", () => {
@@ -20,12 +20,12 @@ test("Check for some component attribute", () => {
 
 test("Check for Increment button", () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='increment-button']");
-  expect(appComponent.length).toBe(1);
+  const incrementButton = wrapper.find("[data-test='increment-button']");
+  expect(incrementButton.length).toBe(1);
 });
 
 test("Check for Increment Button", () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='increment-counter']");
-  expect(appComponent.length).toBe(1);
+  const incrementCounter = wrapper.find("[data-test='increment-counter']");
+  expect(incrementCounter.length).toBe(1);
 });
