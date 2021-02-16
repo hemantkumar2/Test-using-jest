6976d29c546cfd2d3cd4c9a475fc8bd21d62abd9
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-02-16 19:09:25 +0530

    made test cases DRY

--- src/App.test.js
+++ src/App.test.js
@@ -6,26 +6,47 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the app component.
+ * @function setup
+ * @param {object} props
+ * @param {any} state - Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+/**
+ *
+ * @param {shallowWrapper} wrapper
+ * @param {string} val - value of data-test attribute for test.
+ * @returns {ShallowWrapper}
+ */
+const findByTestAdder = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test("Render without error", () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const link = wrapper.find("[href='http://nirmalkar.com']");
   expect(link.length).toBe(1);
 });
 
 test("Check for some component attribute", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='some-component']");
+  const wrapper = setup();
+  const appComponent = findByTestAdder(wrapper, "some-component");
   expect(appComponent.length).toBe(1);
 });
 
 test("Check for Increment button", () => {
-  const wrapper = shallow(<App />);
-  const incrementButton = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const incrementButton = findByTestAdder(wrapper, "increment-button");
   expect(incrementButton.length).toBe(1);
 });
 
 test("Check for Increment Button", () => {
-  const wrapper = shallow(<App />);
-  const incrementCounter = wrapper.find("[data-test='increment-counter']");
+  const wrapper = setup();
+  const incrementCounter = findByTestAdder(wrapper, "increment-counter");
   expect(incrementCounter.length).toBe(1);
 });
